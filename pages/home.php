7823604a935cb48f41a10847ab6b648347550dc5
mouadh <mouadh.kahouli14@gmail.com>
2015-12-12 17:31:18 +0100

    data base part

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,12 @@
-<h1>je suis la Home page</h1>
+<?php
 
-<p><a href="index.php?p=single"></a></p>
+$pdo = new PDO('mysql:dbname=blog;host=localhost','root','');
+
+$pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$res = $pdo -> query('SELECT * FROM article');
+
+//$count = $pdo->exec('INSERT INTO article SET titre="Mon titre",date="'.date('Y-m-d H:i:s').'"');
+
+var_dump($count);
+
--- pages/single.php
+++ pages/single.php
@@ -1,1 +1,3 @@
 <h1>je suis single</h1>
+
+<p><a href="index.php">Page Home</a></p>
